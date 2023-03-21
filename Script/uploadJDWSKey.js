9c991272904da346225e8c42e6139444395d326d
id77 <9592236+id77@users.noreply.github.com>
2023-03-21 21:27:42 +0800

    Update uploadJDWSKey.js

--- Script/uploadJDWSKey.js
+++ Script/uploadJDWSKey.js
@@ -9,14 +9,14 @@
 ===================
 [Script]
 自动上车-id77 = type=http-request,pattern=^https:\/\/api\-dd\.jd\.com\/client\.action\?functionId=getSessionLog,requires-body=1,max-size=0,timeout=1000,script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/Script/uploadJDWSKey.js,script-update-interval=0
-自动上车-id77 = type=http-response,pattern=functionId=newUserInfo,requires-body=1,max-size=0,timeout=1000,script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/Script/uploadJDWSKey.js,script-update-interval=0
+自动上车-id77 = type=http-response,pattern=functionId=getChatSessionLog,requires-body=1,max-size=0,timeout=1000,script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/Script/uploadJDWSKey.js,script-update-interval=0
 
 ===================
 【Loon脚本配置】:
 ===================
 [Script]
 http-request ^https:\/\/api\-dd\.jd\.com\/client\.action\?functionId=getSessionLog tag=自动上车-id77, script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/Script/uploadJDWSKey.js,requires-body=1
-http-response functionId=newUserInfo tag=自动上车-id77, script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/Script/uploadJDWSKey.js,requires-body=1
+http-response functionId=getChatSessionLog tag=自动上车-id77, script-path=https://raw.githubusercontent.com/id77/QuantumultX/master/Script/uploadJDWSKey.js,requires-body=1
 
 ===================
 【 QX  脚本配置 】:
